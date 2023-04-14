7d2e8e9e3e8ff3cb0c2782acb025739793a0a921
DurgaDarba <ddarba@gmail.com>
2023-04-14 00:40:58 -0500

    wrong test

--- tests/testlorenz.cc
+++ tests/testlorenz.cc
@@ -8,10 +8,58 @@
 
 #include "../src/lorenz.h"
 
-// using namespace rgb_matrix;
+using namespace rgb_matrix;
 using namespace std;
 
-// int lorenz(Canvas *canvas) {
+int lorenz(Canvas *canvas) {
+    Lorenz lorenz;
+    // cout << lorenz.getX() << " " << lorenz.getY() << " " << lorenz.getZ() << endl;
+
+    vector<vector<float> > grid(64, vector<float>(64));
+
+    for (int i = 0; i < 64; i++) {
+        for (int j = 0; j < 64; j++) {
+            grid[i][j] = 0;
+        }
+    }
+
+    while(true) {
+        // cout << lorenz.getX() << " " << lorenz.getY() << " " << lorenz.getZ() << endl;
+        lorenz.generateFrames(grid);
+
+        // print the grid
+        for(int y = 0; y < 64; y++) {
+            for(int x = 0; x < 64; x++) {
+                // print grid where x is the first index and y is the second index and if z is greater than 0 then print a 1
+                float value = grid[y][x];
+                
+                float r = 255 * value;
+                float g = 255 * value;
+                float b = 255 * value;
+                
+                canvas->SetPixel(x, y, r, g, b);
+            }
+        }
+    }
+}
+
+int main(int argc, char *argv[]) {
+    RGBMatrix::Options defaults;
+    defaults.hardware_mapping = "adafruit-hat-pwm";  // or e.g. "adafruit-hat"
+    defaults.rows = 64;
+    defaults.cols = 64;
+
+    Canvas *canvas = RGBMatrix::CreateFromFlags(&argc, &argv, &defaults);
+    if (canvas == NULL)
+    return 1;
+
+    lorenz(canvas);    // Using the canvas.
+    return 0;
+
+}
+
+
+// int main(int argc, char *argv[]) {
 //     Lorenz lorenz;
 //     // cout << lorenz.getX() << " " << lorenz.getY() << " " << lorenz.getZ() << endl;
 
@@ -37,25 +85,11 @@ using namespace std;
 //                 float g = 255 * value;
 //                 float b = 255 * value;
                 
-//                 canvas->SetPixel(x, y, r, g, b);
+//                 cout << r << " " << g << " " << b << endl;
 //             }
 //         }
 //     }
-// }
-
-// int main(int argc, char *argv[]) {
-//     RGBMatrix::Options defaults;
-//     defaults.hardware_mapping = "adafruit-hat-pwm";  // or e.g. "adafruit-hat"
-//     defaults.rows = 64;
-//     defaults.cols = 64;
-
-//     Canvas *canvas = RGBMatrix::CreateFromFlags(&argc, &argv, &defaults);
-//     if (canvas == NULL)
-//     return 1;
-
-//     lorenz(canvas);    // Using the canvas.
 //     return 0;
-
 // }
 
 
@@ -65,28 +99,9 @@ int main(int argc, char *argv[]) {
 
     vector<vector<float> > grid(64, vector<float>(64));
 
-    // for (int i = 0; i < 64; i++) {
-    //     for (int j = 0; j < 64; j++) {
-    //         grid[i][j] = 0;
-    //     }
-    // }
-    // preinitialize the grid to lorentz attractor
-    int t = 0;
-    while(t < 100) {
-        lorenz.lorenz();
-        t++;
-    
-        for (int i = 0; i < 64; i++) {
-            for (int j = 0; j < 64; j++) {
-
-                int x1 = lorenz.getX() + 32;
-                int y1 = lorenz.getY() + 32;
-                int z1 = lorenz.getZ();
-                
-                if (x1 >= 0 && x1 < 64 && z1 >= 0 && z1 < 64) {
-                    grid[z1][x1] = 1;
-                }
-            }
+    for (int i = 0; i < 64; i++) {
+        for (int j = 0; j < 64; j++) {
+            grid[i][j] = 0;
         }
     }
 
@@ -95,21 +110,18 @@ int main(int argc, char *argv[]) {
         lorenz.generateFrames(grid);
 
         // print the grid
-        for(int x = 0; x < 64; x++) {
-            for(int y = 0; y < 64; y++) {
-                // get the grid value and convert it to a color simialr to the state machine
-                float value = grid[x][y];
-
-                float r = 255 * powf(value, 4 + (value * 0.5)) * cosf(value);
-                float g = 255 * powf(value, 3 + (value * 0.5)) * sinf(value);
-                float b = 255 * powf(value, 2 + (value * 0.5));
+        for(int y = 0; y < 64; y++) {
+            for(int x = 0; x < 64; x++) {
+                // print grid where x is the first index and y is the second index and if z is greater than 0 then print a 1
+                float value = grid[y][x];
+                
+                float r = 255 * value;
+                float g = 255 * value;
+                float b = 255 * value;
                 
                 cout << r << " " << g << " " << b << endl;
             }
         }
-
-        // 30 frames per second
-        usleep(33333);
     }
     return 0;
 }
