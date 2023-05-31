d2c2e9d8b36a43064bbd09607f59b3531bf7314a
DurgaDarba <ddarba@gmail.com>
2023-05-31 00:59:59 -0500

    clearning up

--- tests/testlorenz.cc
+++ tests/testlorenz.cc
@@ -11,39 +11,6 @@
 using namespace rgb_matrix;
 using namespace std;
 
-// int lorenz(Canvas *canvas) {
-//     Lorenz lorenz;
-//     // cout << lorenz.getX() << " " << lorenz.getY() << " " << lorenz.getZ() << endl;
-
-//     vector<vector<float> > grid(64, vector<float>(64));
-
-//     for (int i = 0; i < 64; i++) {
-//         for (int j = 0; j < 64; j++) {
-//             grid[i][j] = 0;
-//         }
-//     }
-
-//     while(true) {
-//         cout << lorenz.getX() << " " << lorenz.getY() << " " << lorenz.getZ() << endl;
-//         lorenz.generateFrames(grid);
-
-//         // print the grid
-//         for(int x = 0; x < 64; x++) {
-//             for(int y = 0; y < 64; y++) {
-//                 // print grid where x is the first index and y is the second index and if z is greater than 0 then print a 1
-//                 float value = grid[x][y];
-                
-//                 float r = 255 * powf(value, 4 + (value * 0.5)) * cosf(value);
-//                 float g = 255 * powf(value, 3 + (value * 0.5)) * sinf(value);
-//                 float b = 255 * powf(value, 2 + (value * 0.5));
-                
-//                 canvas->SetPixel(x, y, r, g, b);
-//             }
-//         }
-//         usleep(33333);
-//     }
-// }
-
 
 int lorenz(Canvas *canvas) {
     Lorenz lorenz;
@@ -51,11 +18,6 @@ int lorenz(Canvas *canvas) {
 
     vector<vector<float> > grid(64, vector<float>(64));
 
-    // for (int i = 0; i < 64; i++) {
-    //     for (int j = 0; j < 64; j++) {
-    //         grid[i][j] = 0;
-    //     }
-    // }
     // preinitialize the grid to lorentz attractor
     int t = 0;
     while(t < 500) {
