0400c4d1020e4f95159733207761874195848c8e
DurgaDarba <ddarba@gmail.com>
2023-06-13 18:08:08 -0500

    chat got here we go lmao

--- tests/testspotify.cc
+++ tests/testspotify.cc
@@ -2,29 +2,30 @@
 #include <iostream>
 #include <unistd.h>
 #include <Magick++.h>
+#include <led-matrix.h>
 
 #include "../src/spotify.h"
-#include <rgbmatrix/graphics.h>
 
 using namespace std;
-using namespace rgb_matrix;
+using rgb_matrix::Canvas;
+using rgb_matrix::RGBMatrix;
+using rgb_matrix::FrameCanvas;
 using ImageVector = std::vector<Magick::Image>;
 
 // Given the filename, load the image and scale it to the size of the matrix.
 static ImageVector LoadImageAndScaleImage(const char *filename, int target_width, int target_height) {
     ImageVector result;
-
     ImageVector frames;
+
     try {
         Magick::readImages(&frames, filename);
-    } catch (std::exception &e) {
-        if (e.what())
-            fprintf(stderr, "%s\n", e.what());
+    } catch (Magick::Exception &e) {
+        std::cerr << "Error loading image: " << e.what() << std::endl;
         return result;
     }
 
     if (frames.empty()) {
-        fprintf(stderr, "No image found.");
+        std::cerr << "No image found." << std::endl;
         return result;
     }
 
@@ -32,7 +33,7 @@ static ImageVector LoadImageAndScaleImage(const char *filename, int target_width
     if (frames.size() > 1) {
         Magick::coalesceImages(&result, frames.begin(), frames.end());
     } else {
-        result.push_back(frames[0]); // just a single still image.
+        result.push_back(frames[0]); // just a single still image
     }
 
     for (Magick::Image &image : result) {
@@ -42,41 +43,38 @@ static ImageVector LoadImageAndScaleImage(const char *filename, int target_width
     return result;
 }
 
-void CopyImageToCanvas(const Magick::Image &image, FrameCanvas *canvas) {
+void CopyImageToCanvas(const Magick::Image &image, Canvas *canvas) {
     const int offset_x = 0, offset_y = 0; // If you want to move the image.
     // Copy all the pixels to the canvas.
-    for (ssize_t y = 0; y < image.rows(); ++y) {
-        for (ssize_t x = 0; x < image.columns(); ++x) {
+    for (size_t y = 0; y < image.rows(); ++y) {
+        for (size_t x = 0; x < image.columns(); ++x) {
             const Magick::Color &c = image.pixelColor(x, y);
             if (c.alphaQuantum() < 256) {
                 canvas->SetPixel(x + offset_x, y + offset_y,
-                                 ScaleQuantumToChar(c.redQuantum()),
-                                 ScaleQuantumToChar(c.greenQuantum()),
-                                 ScaleQuantumToChar(c.blueQuantum()));
+                                 c.redQuantum(), c.greenQuantum(), c.blueQuantum());
             }
         }
     }
 }
 
 int main(int argc, char *argv[]) {
-    Spotify spotify = Spotify();
-
     RGBMatrix::Options defaults;
     defaults.hardware_mapping = "adafruit-hat-pwm"; // or e.g. "adafruit-hat"
     defaults.rows = 64;
     defaults.cols = 64;
 
-    Canvas *canvas = RGBMatrix::CreateFromFlags(&argc, &argv, &defaults);
-    if (canvas == nullptr) {
-        fprintf(stderr, "Failed to create RGBMatrix canvas.\n");
+    RGBMatrixRuntime matrix_runtime;
+    if (!matrix_runtime.Init(argc, argv, defaults))
         return 1;
-    }
 
-    FrameCanvas *frame_canvas = dynamic_cast<FrameCanvas *>(canvas);
-    if (frame_canvas == nullptr) {
-        fprintf(stderr, "Failed to get FrameCanvas.\n");
+    SignalHandler signal_handler;
+    signal_handler.SetupExitCallback([&]() { matrix_runtime.Stop(); });
+
+    Canvas *canvas = matrix_runtime.matrix();
+    if (canvas == nullptr)
         return 1;
-    }
+
+    Spotify spotify;
 
     while (true) {
         int updated = spotify.update();
@@ -84,17 +82,17 @@ int main(int argc, char *argv[]) {
         if (spotify.isPlaying()) {
             if (updated) {
                 // Load the image
-                ImageVector images = LoadImageAndScaleImage("./tmp/spotify.png", 64, 64);
-                if (!images.empty()) {
-                    CopyImageToCanvas(images[0], frame_canvas);
-                    frame_canvas->Update();
-                }
+                ImageVector image = LoadImageAndScaleImage("./tmp/spotify.png", 64, 64);
+                CopyImageToCanvas(image[0], canvas);
             }
-            sleep(4);
+            sleep(5);
             continue;
         }
+
+        sleep(10);
     }
 
-    delete canvas;
+    matrix_runtime.Stop();
+
     return 0;
 }
